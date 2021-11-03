7dca7b4db0ff10ff35cb51e4836d33be3b662beb
Eric Regnier <eric.regnier.mail@gmail.com>
2021-11-03 23:50:36 +0100

    feat: reactive markers with position update on marker data change

--- vue-leaflet-custom-marker.vue
+++ vue-leaflet-custom-marker.vue
@@ -1,96 +1,110 @@
 <template>
-  <div class="marker-wrapper" >
+  <div class="marker-wrapper">
     <slot />
   </div>
 </template>
 <script>
-import L from "leaflet";
-import { Icon } from "leaflet/src/layer/marker/Icon";
-import { findRealParent } from "vue2-leaflet/src/utils/utils.js";
+import L from 'leaflet'
+import { Icon } from 'leaflet/src/layer/marker/Icon'
+import { findRealParent } from 'vue2-leaflet/src/utils/utils.js'
 
 export default {
   props: {
     marker: {
       type: Object,
-      default: undefined,
+      default: undefined
     },
     alignment: {
       type: String,
-      default: "center",
+      default: 'center'
     },
     offsetX: {
       type: Number,
-      default: 0,
+      default: 0
     },
     offsetY: {
       type: Number,
-      default: 0,
-    },
+      default: 0
+    }
   },
-  mounted() {
-    L.marker([this.marker.lat, this.marker.lng], {
-      icon: new CustomMarker({ html: this.$el }),
-    }).addTo(findRealParent(this.$parent).mapObject);
-    this.computeAlignment(this.alignment);
+  data () {
+    return {
+      leafMarker: undefined
+    }
+  },
+  mounted () {
+    this.leafMarker = L.marker([this.marker.lat, this.marker.lng], {
+      icon: new CustomMarker({ html: this.$el })
+    })
+    this.leafMarker.addTo(findRealParent(this.$parent).mapObject)
+    this.computeAlignment(this.alignment)
   },
   watch: {
-    alignment(alignment) {
-      this.computeAlignment(alignment);
+    alignment (alignment) {
+      this.computeAlignment(alignment)
     },
+    marker (marker) {
+      this.leafMarker.setLatLng(marker)
+    }
   },
   methods: {
-    computeAlignment(alignment) {
-      const div = this.$el;
-      let x = 0,
-        y = 0;
+    computeAlignment (alignment) {
+      const div = this.$el
+      let x = 0
+      let y = 0
       switch (alignment) {
-        case "top":
-          y -= div.offsetHeight;
-          x -= div.offsetHeight / 2;
-          break;
-        case "bottom":
-          x = x - div.offsetWidth / 2;
-          break;
-        case "left":
-          x = x - div.offsetWidth;
-          y = y - div.offsetHeight / 2;
-          break;
-        case "right":
-          y = y - div.offsetHeight / 2;
-          break;
-        case "center":
-          x -= div.offsetWidth / 2;
-          y -= div.offsetHeight / 2;
-          break;
-        case "topleft":
-          x = -div.offsetWidth;
-          y = -div.offsetHeight;
-          break;
-        case "topright":
-          y = y - div.offsetHeight;
-          break;
-        case "bottomleft":
-          x = x - div.offsetWidth;
-          break;
-        case "bottomright":
-          break;
+        case 'top':
+          y -= div.offsetHeight
+          x -= div.offsetHeight / 2
+          break
+        case 'bottom':
+          x = x - div.offsetWidth / 2
+          break
+        case 'left':
+          x = x - div.offsetWidth
+          y = y - div.offsetHeight / 2
+          break
+        case 'right':
+          y = y - div.offsetHeight / 2
+          break
+        case 'center':
+          x -= div.offsetWidth / 2
+          y -= div.offsetHeight / 2
+          break
+        case 'topleft':
+          x = -div.offsetWidth
+          y = -div.offsetHeight
+          break
+        case 'topright':
+          y = y - div.offsetHeight
+          break
+        case 'bottomleft':
+          x = x - div.offsetWidth
+          break
+        case 'bottomright':
+          break
         default:
-          throw new Error("Invalid alignment type of custom marker!");
+          throw new Error('Invalid alignment type of custom marker!')
       }
-      div.style.left = x + this.offsetX + "px";
-      div.style.top = y + this.offsetY + "px";
-    },
+      div.style.left = x + this.offsetX + 'px'
+      div.style.top = y + this.offsetY + 'px'
+    }
   },
-};
+  beforeDestroy () {
+    if (this.leafMarker) {
+      this.leafMarker.remove()
+    }
+  }
+}
 
 export var CustomMarker = Icon.extend({
   options: {},
   createIcon: function () {
-    var div = document.createElement("div");
-    div.appendChild(this.options.html);
-    return div;
-  },
-});
+    const div = document.createElement('div')
+    div.appendChild(this.options.html)
+    return div
+  }
+})
 </script>
 <style scoped>
 .marker-wrapper {
